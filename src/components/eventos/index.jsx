061254ce199125381d3aed15c341935d10365936
Felipe Fernandes <f.fernandescs@gmail.com>
2022-06-21 05:56:27 -0300

    Projeto em conclusão1

--- src/components/eventos/index.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const Eventos = () => {
-    return (
-            <div class="spinner-border" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-    )
-}
